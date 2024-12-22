6dec622b5dce5ad77a42ec637cf03b27004d8331
Nemertz012 <nemertz012@yandex.ru>
2024-12-22 10:38:03 +0300

    nemertz.lua

--- nemertz.lua
+++ nemertz.lua
@@ -1,1 +1,19 @@
-print("hello world")
+local LegacyRAWINP = ("https://raw.githubusercontent.com/ScreamerUWU/ScreamerHUB/main/Games/YBA/HUB/Legacy/Index.lua")
+local LegacyMethod = ("GET")
+local LegacyHeaders = {}
+
+local Success, Error = pcall(function()
+    local RequestData = {
+     Url = (LegacyRAWINP),
+     Method = (LegacyMethod),
+     Headers = (LegacyHeaders)
+    }
+      
+    loadstring(syn.request(RequestData).Body)()
+end)
+
+if not Success then
+   local Dep = game:HttpGet(LegacyRAWINP)
+   
+   loadstring(Dep)()
+end
